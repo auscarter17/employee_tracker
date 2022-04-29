d0e76aa5d5c9bdbaae85e961e3cd86d7b7a5e440
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-04-28 21:24:29 -0400

    created tables

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) ON DELETE SET NULL
+);
+
+CRATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    CONSTRAINT fk_role FOREIGN KEY (department_id) ON DELETE SET NULL
+);
